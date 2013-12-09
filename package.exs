1b8204e30fb67920f16b2a78a0f033ac095f700a
Duncan McGreggor <oubiwann@gmail.com>
2013-12-08 21:48:42 -0600

    Added EXPM file.

---
+++ package.exs
@@ -0,0 +1,7 @@
+Expm.Package.new(name: "lfeunit",
+                 description: "An eunit clone for LFE",
+                 version: "0.0.1",
+                 keywords: ["LFE", "Lisp", "Testing"],
+                 maintainers: [[name: "Duncan McGreggor",
+                                email: "oubiwann@cogitat.io"]],
+                 repositories: [[github: "lfe/lfeunit"]])

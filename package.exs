c7164bd4d634162dbea3c25ce588281d9dd0ea3c
Duncan McGreggor <duncan@cogitat.io>
2014-04-01 10:49:36 -0700

    Bumped version number (0.1.1).

--- package.exs
+++ package.exs
@@ -1,6 +1,6 @@
 Expm.Package.new(name: "lfeunit",
                  description: "An eunit clone for LFE",
-                 version: "0.1.0",
+                 version: "0.1.1",
                  keywords: ["LFE", "Lisp", "Testing", "Unit Tests", "TDD"],
                  maintainers: [[name: "Duncan McGreggor",
                                 email: "oubiwann@cogitat.io"]],

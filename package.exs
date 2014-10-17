44f05b173a09a29bb165aff96d35a4afff5b7971
Døkkarr Hirðisson <dokkarr@lfe.io>
2014-10-16 23:13:12 -0500

    Updated to version 0.3.2.
    This version switches to using an explicit lutil version (0.2.9).

--- package.exs
+++ package.exs
@@ -1,6 +1,6 @@
 Expm.Package.new(name: "ltest",
                  description: "A Testing Framework for LFE",
-                 version: "0.3.1",
+                 version: "0.3.2",
                  keywords: ["LFE", "Lisp", "Testing", "Unit Tests", "TDD"],
                  maintainers: [[name: "Duncan McGreggor",
                                 email: "oubiwann@cogitat.io"]],

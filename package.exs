b3d545eeedb63d6330f228b1fc7ac91c04d34f36
Duncan M. McGreggor <oubiwann@cogitat.io>
2015-01-04 16:16:11 -0600

    Bumped version to 0.4.1.

--- package.exs
+++ package.exs
@@ -1,6 +1,6 @@
 Expm.Package.new(name: "ltest",
                  description: "A Testing Framework for LFE",
-                 version: "0.4.0",
+                 version: "0.4.1",
                  keywords: ["LFE", "Lisp", "Testing", "Unit Tests", "TDD"],
                  maintainers: [[name: "Duncan McGreggor",
                                 email: "oubiwann@cogitat.io"]],

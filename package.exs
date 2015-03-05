c0763504c07bdc9a63dda4f72c54dd1c6277438c
Cinova Dev Team <dev@cinova.co>
2015-03-05 13:39:52 -0600

    Removed exs file.
    Elixir community no longer supports expm.

--- package.exs
+++
@@ -1,7 +0,0 @@
-Expm.Package.new(name: "ltest",
-                 description: "A Testing Framework for LFE",
-                 version: "0.4.2",
-                 keywords: ["LFE", "Lisp", "Testing", "Unit Tests", "TDD"],
-                 maintainers: [[name: "Duncan McGreggor",
-                                email: "oubiwann@cogitat.io"]],
-                 repositories: [[github: "lfex/ltest"]])

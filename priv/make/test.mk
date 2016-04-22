ca0377d6648a4723d3162742c2c86c0652683458
Eric Bailey <eric@ericb.me>
2016-04-22 00:20:43 -0500

    Fix make check-runner-*
    Use an explicit `_build/test/lib/*/ebin` and avoid backticks.

--- priv/make/test.mk
+++ priv/make/test.mk
@@ -1,20 +1,26 @@
+CWD := $(CURDIR)
+
+compile-tests:
+	@rebar3 as test do clean, compile
+
+check-runner-ltest: export CODE_PATH = _build/test/lib/*/ebin
 check-runner-ltest: compile
 	@clear
-	@erl -pa `rebar3 path -s " -pa "` \
-	-cwd "`pwd`" -listener ltest-listener -eval \
+	@erl -pa ${CODE_PATH} -cwd "${CWD}" -listener ltest-listener -eval \
 	"case 'ltest-runner':all() of ok -> halt(0); _ -> halt(127) end" \
 	-noshell
 
+check-runner-ltest-no-color: export CODE_PATH = _build/test/lib/*/ebin
 check-runner-ltest-no-color: compile
 	@clear
-	@erl -pa `rebar3 path -s " -pa "` \
-	-cwd "`pwd`" -listener ltest-listener -color false -eval \
+	@erl -pa ${CODE_PATH} \
+	-cwd "${CWD}" -listener ltest-listener -color false -eval \
 	"case 'ltest-runner':all() of ok -> halt(0); _ -> halt(127) end" \
 	-noshell
 
+check-runner-eunit: export CODE_PATH = _build/test/lib/*/ebin
+check-runner-eunit: compile-tests
 check-runner-eunit: compile
-	@erl -pa `rebar3 path -s " -pa "` \
-	-cwd "`pwd`" -listener eunit_progress -eval \
+	@erl -pa ${CODE_PATH} -cwd "${CWD}" -listener eunit_progress -eval \
 	"case 'ltest-runner':all() of ok -> halt(0); _ -> halt(127) end" \
 	-noshell
-

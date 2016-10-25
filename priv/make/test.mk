2f14f2fb9ab79182efeea9db806bd046180c34b3
Duncan McGreggor <oubiwann@gmail.com>
2016-10-25 12:58:09 -0500

    Added missing deps dirs.

--- priv/make/test.mk
+++ priv/make/test.mk
@@ -3,14 +3,14 @@ CWD := $(CURDIR)
 compile-tests:
 	@rebar3 as test do clean, compile
 
-check-runner-ltest: export CODE_PATH = _build/test/lib/*/ebin _build/test/plugins/*/ebin
+check-runner-ltest: export CODE_PATH = _build/test/lib/*/ebin _build/test/lib/*/ebin _build/test/plugins/*/ebin
 check-runner-ltest: compile-tests
 	@clear
 	@erl -pa ${CODE_PATH} -cwd "${CWD}" -listener ltest-listener -eval \
 	"case 'ltest-runner':all() of ok -> halt(0); _ -> halt(127) end" \
 	-noshell
 
-check-runner-ltest-no-color: export CODE_PATH = _build/test/lib/*/ebin
+check-runner-ltest-no-color: export CODE_PATH = _build/test/lib/*/ebin _build/test/lib/*/ebin
 check-runner-ltest-no-color: compile-tests
 	@clear
 	@erl -pa ${CODE_PATH} \
@@ -18,7 +18,7 @@ check-runner-ltest-no-color: compile-tests
 	"case 'ltest-runner':all() of ok -> halt(0); _ -> halt(127) end" \
 	-noshell
 
-check-runner-eunit: export CODE_PATH = _build/test/lib/*/ebin
+check-runner-eunit: export CODE_PATH = _build/test/lib/*/ebin _build/default/lib/*/ebin
 check-runner-eunit: compile-tests
 	@erl -pa ${CODE_PATH} -cwd "${CWD}" -listener eunit_progress -eval \
 	"case 'ltest-runner':all() of ok -> halt(0); _ -> halt(127) end" \
